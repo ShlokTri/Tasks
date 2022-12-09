62be7362e4ea613c6149ee3d84ca1dbb88c73374
Shlok Trivedi <90284812+ShlokTri@users.noreply.github.com>
2022-12-09 14:30:58 +0530

    Create variables.tf

---
+++ variables.tf
@@ -0,0 +1,16 @@
+variable "tenant_id" {
+  type ="number"
+  description = "This is the tenant Id"
+}
+variable "client_id" {
+  type ="number"
+  description = "This is the client id"
+}
+variable "subscription_id" {
+  type ="number"
+  description = "This is the subscription Id"
+}
+variable "client_secret" {
+  type ="number"
+  description = "This is the client Id"
+}
